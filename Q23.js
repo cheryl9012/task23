18db7d4e29011c5565a8d4c996668d375da29a77
cheryl9012 <161365360+cheryl9012@users.noreply.github.com>
2024-03-23 21:32:57 +0500

    Add files via upload

---
+++ Q23.js
@@ -0,0 +1,47 @@
+"use strict";
+// Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+// let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+// console.log(car == 'subaru')
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+// 1st true condition
+let car = 'sabaru';
+console.log("Is car --'subaru'? I predict True. otherwise  false");
+console.log(car == 'sabaru');
+//  2nd true condition
+let bike = 'Honda';
+console.log("Is bike --Honda? I predict true otherwise false ");
+console.log(bike == 'Honda');
+// 3rd true condition
+let pen = 'Dollar';
+console.log("Is pen -- 'Dollar? I predict true. otherwise false");
+console.log(pen == 'Dollar');
+//  4th true condition
+let fruit = 'Orange';
+console.log("Is fruit --'orange'? I predict true. otherwise false");
+console.log(fruit == 'Orange');
+// 5th true condition
+let color = 'Red';
+console.log(" Is color --'Red'? I predict true. otherwise false");
+console.log(color == 'Yellow');
+// 6th false conditon
+let computer = ' Hp';
+console.log("Is computer -- 'Hp'? I predict true otherwise false");
+console.log(computer == 'Dell');
+// 7th false condition
+let dog = 'Huskey';
+console.log("Is dog -- 'Huskey'? I predict true.otherwise false");
+console.log(dog == 'Afghan Kuchi');
+// 8th false conditon
+let watch = 'Rolex';
+console.log("Is watch --'Rolex'? I predict true.otherwise false");
+console.log(watch == 'Omega');
+// 9th false condition
+let season = 'Winter';
+console.log(" Is season -- 'winter'? I predict true. otherwise false");
+console.log(season == 'Summer');
+// 10th condition
+let animal = 'Elephant';
+console.log("Is animal --'Elephant'? I predict true");
+console.log(animal == 'Lion');
